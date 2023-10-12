6e38760154835dffe639e46aee8e42b2f485588b
Ayo-mi <ajayijubrilayo@gmail.com>
2023-10-12 20:32:12 +0100

    logic added

--- app/src/main/java/com/example/savingscalculator/MainActivity.kt
+++ app/src/main/java/com/example/savingscalculator/MainActivity.kt
@@ -1,27 +1,44 @@
 package com.example.savingscalculator
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.example.savingscalculator.databinding.ActivityMainBinding
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainView : ActivityMainBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mainView = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainView.root)
 
-        try {
-            mainView.compute.setOnClickListener {
+        mainView.compute.setOnClickListener {
+            try {
+                val target = mainView.target.text.toString().toFloat()
+                val save = mainView.save.text.toString().toFloat()
+                if (target < save) {
+                    Toast.makeText(
+                        this,
+                        "Target savings can't br lesser than amount save yearly",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                val dec = DecimalFormat("#,###.00")
 
+                var years = (target / save).toInt()
+                mainView.result.text =
+                    "It will take approx. $years years to save " +
+                            "$${dec.format(target)}\nwith a yearly savings of $${dec.format(save)}"
                 closeKeyBoard()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Invalid action", Toast.LENGTH_LONG).show()
             }
-        }catch (e : Exception){
-            Toast.makeText(this, "Invalid action", Toast.LENGTH_LONG).show()
         }
     }
 
